5f21b42044ba585876d731c176038303157ffdf4
Andreas Aigner <A3igner@icloud.com>
2020-12-28 07:31:26 +0100

    Initial Commit

---
+++ TextShadow/TextShadowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextShadowApp.swift
+//  TextShadow
+//
+//  Created by a3 on 28.12.20.
+//
+
+import SwiftUI
+
+@main
+struct TextShadowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
